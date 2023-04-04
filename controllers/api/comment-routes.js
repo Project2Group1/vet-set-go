cd0807aa8a1ebda09c08c0c856cebd71eae927a6
Pholobo <ouyangphoenix@gmail.com>
2023-04-03 20:18:01 -0400

    adds get route for comments

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -2,3 +2,19 @@ const router = require('express').Router();
 const { Comments } = require('../../models');
 const withAuth = require('../../utils/auth');
 
+// route to get all comments
+router.get('/:id', withAuth, async (req, res) => {
+	try {
+		const commentData = await Comments.findByPk(req.params.id, {
+			include: [{ model: Users, attributes: ['firstName', 'lastName']}],
+		});
+		if (!commentData) {
+			res.status(404).json({ message: 'Sorry, that comment is not available.' });
+			return;
+		}
+		res.status(200).json(commentData)
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
