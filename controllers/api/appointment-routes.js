48d5b9a982f584bf1643fc2b5698c912a985c1ce
Julia B <julia.bernst@gmail.com>
2023-04-07 18:15:25 -0400

    Added routes to get client and guest appointments

--- controllers/api/appointment-routes.js
+++ controllers/api/appointment-routes.js
@@ -1,6 +1,36 @@
 const router = require("express").Router();
 const { Appointments } = require("../../models");
 
+// GET guest appointment page
+router.get("/guest-appointment", async (req, res) => {
+  try {
+    res.render("guest-appointment");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET client appointment page
+router.get("/client-appointment", async (req, res) => {
+  try {
+    res.render("client-appointment");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// GET Login route
+router.get("/login", (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 // CREATE new appointment
 router.post("/", (req, res) => {
   try {
