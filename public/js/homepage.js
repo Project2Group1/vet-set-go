18a67d5a954667c0fb3d84e70ad391de5101d742
mhamze23 <hamze.mohamed@live.ca>
2023-04-10 17:43:41 -0400

    Few improvements on the homepage

---
+++ public/js/homepage.js
@@ -0,0 +1,32 @@
+const carousel = (function() {
+  let currentIndex = 0;
+  let items;
+  let totalItems;
+
+  function changeItem() {
+    items.forEach(item => item.classList.remove('is-active'));
+    items[currentIndex].classList.add('is-active');
+  }
+
+  function nextItem() {
+    currentIndex = (currentIndex + 1) % totalItems;
+    changeItem();
+  }
+
+  function autoRotate() {
+    setInterval(nextItem, 4000); // Adjust the time interval in milliseconds for automatic rotation
+  }
+
+  return {
+    init: function() {
+      items = document.querySelectorAll('.carousel .carousel-item');
+      totalItems = items.length;
+      autoRotate();
+    }
+  };
+})();
+
+document.addEventListener('DOMContentLoaded', function() {
+  carousel.init();
+});
+
