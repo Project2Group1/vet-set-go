b30c9b60900f429971bcef4577067cb4123602fa
Julia B <julia.bernst@gmail.com>
2023-04-12 11:02:19 -0400

    Split getPets function between router and controller

--- controllers/api/appointment-routes.js
+++ controllers/api/appointment-routes.js
@@ -1,29 +1,76 @@
 const router = require("express").Router();
-const { Appointments } = require("../../models");
+const { Appointments, Users, Pets } = require("../../models");
+
+// GET names of current user's pets
+router.get("/", async (req, res) => {
+  try {
+    const dbpetNames = await Pets.findAll({
+      where: { owner_id: req.session.user_id },
+      attributes: ["name"],
+    });
+
+    console.log(dbpetNames);
+    res.status(200).json(dbpetNames);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 // CREATE new appointment
 router.post("/", async (req, res) => {
-  try {
-    const newAppointment = await Appointments.create({
-      user_id: req.body.user_id,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      pet_name: req.body.pet_name,
-      petType: req.body.type,
-      breed: req.body.breed,
-      allergies: req.body.allergies,
-      vaccinated: req.body.vaccinated,
-      birthday: req.body.birthday,
-      isNeuteredOrSpayed: req.body.spayNeutered,
-      sex: req.body.sex,
-      concern: req.body.concern,
+  // If the appointment is for an existing user
+  if (req.body.isUser) {
+    const userData = await Users.findByPk(req.session.user_id);
+    const petData = await Pets.findOne({
+      where: { user_id: userData.id, name: req.body.pet_name },
     });
 
-    res.status(200).json(newAppointment);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+    try {
+      const newAppointment = await Appointments.create({
+        user_id: userData.user_id,
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        email: userData.email,
+        pet_name: petData.name,
+        petType: petData.petType,
+        breed: petData.breed,
+        allergies: petData.allergies,
+        vaccinated: petData.vaccinated,
+        birthday: petData.birthday,
+        isNeuteredOrSpayed: petData.isNeuteredOrSpayed,
+        sex: petData.sex,
+        concern: req.body.concern,
+      });
+
+      res.status(200).json(newAppointment);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  } else {
+    // If the appointment is for a guest
+    try {
+      const newAppointment = await Appointments.create({
+        user_id: req.body.user_id,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        pet_name: req.body.pet_name,
+        petType: req.body.type,
+        breed: req.body.breed,
+        allergies: req.body.allergies,
+        vaccinated: req.body.vaccinated,
+        birthday: req.body.birthday,
+        isNeuteredOrSpayed: req.body.spayNeutered,
+        sex: req.body.sex,
+        concern: req.body.concern,
+      });
+
+      res.status(200).json(newAppointment);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   }
 });
 
--- public/js/appointment.js
+++ public/js/appointment.js
@@ -1,5 +1,6 @@
 const session = require("express-session");
 const { Users, Pets } = require("../../models");
+// Move model stuff to route
 
 // Using datepicker for birthday input //
 var options = {
@@ -43,109 +44,83 @@ if (element) {
 
 // Generate a client's pets as dropdown options
 const getPets = async () => {
-  const { petNames, count } = await Pets.findAndCountAll({
-    where: { user_id: session.user_id },
-    attributes: ["name"],
+  const petNames = await fetch("/api/appointment/", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
   });
+  const select = document.getElementById("petName-contact");
 
-  if (count > 0) {
-    const el = document.getElementById("petName-contact");
-    for (var i = 0; i < count; i++) {
-      var option = document.createElement("option");
-      option.text = petNames[i];
-      option.value = petNames[i];
-      el.appendChild(option);
-    }
+  for (var name in petNames) {
+    var option = document.createElement("option");
+    option.text = name;
+    option.value = name;
+    select.appendChild(option);
   }
 };
 
-// Same POST request for both forms
-const sendRequest = async (
-  user_id,
-  firstName,
-  lastName,
-  email,
-  pet_name,
-  type,
-  breed,
-  allergies,
-  vaccinated,
-  birthday,
-  isNeuteredOrSpayed,
-  sex,
-  concern
-) => {
-  const response = await fetch("/api/appointment", {
-    method: "POST",
-    body: JSON.stringify({
-      user_id,
+// Form handler for a client's appointment
+const clientFormHandler = async (event) => {
+  event.preventDefault();
+
+  const pet_name = document.querySelector("#petName-contact").value.trim();
+  const concern = document.querySelector("#concern-contact").value.trim();
+
+  if (pet_name && concern) {
+    // Declare request variables
+    const isUser = true;
+    const {
       firstName,
       lastName,
       email,
-      pet_name,
-      type,
+      petType,
       breed,
       allergies,
       vaccinated,
       birthday,
       isNeuteredOrSpayed,
       sex,
-      concern,
-    }),
-    headers: { "Content-Type": "application/json" },
-  });
+    } = null;
 
-  if (response.ok) {
-    document.getElementById("inner-content").innerHTML =
-      "Your appointment request has been received! Keep an eye on your email for updates.";
-  } else {
-    alert("Failed to submit appointment");
-  }
-};
-
-const clientFormHandler = async (event) => {
-  event.preventDefault();
-
-  try {
-    const pet_name = document.querySelector("#petName-contact").value.trim();
-    const concern = document.querySelector("#concern-contact").value.trim();
-
-    const userData = await Users.findByPk(session.user_id);
-    const petData = await Pets.findOne({
-      where: { user_id: userData.id, name: pet_name },
+    const response = await fetch("/api/appointment/", {
+      method: "POST",
+      body: JSON.stringify({
+        isUser,
+        firstName,
+        lastName,
+        email,
+        pet_name,
+        petType,
+        breed,
+        allergies,
+        vaccinated,
+        birthday,
+        isNeuteredOrSpayed,
+        sex,
+        concern,
+      }),
+      headers: { "Content-Type": "application/json" },
     });
 
-    if (userData && petData && concern) {
-      sendRequest(
-        userData.id,
-        userData.firstName,
-        userData.lastName,
-        userData.email,
-        petData.name,
-        petData.petType,
-        petData.breed,
-        petData.allergies,
-        petData.vaccinated,
-        petData.birthday,
-        petData.isNeuteredOrSpayed,
-        petData.sex,
-        concern
-      );
+    if (response.ok) {
+      document.getElementById("inner-content").innerHTML =
+        "Your appointment request has been received! Keep an eye on your email for updates.";
+    } else {
+      alert("Failed to submit appointment");
     }
-  } catch (err) {
+  } else {
     alert("You are missing one or more required fields");
   }
 };
 
+// Form handler for a guest's appointment
 const guestFormHandler = async (event) => {
   event.preventDefault();
 
-  const user_id = null;
   const firstName = document.querySelector("#firstName-contact").value.trim();
   const lastName = document.querySelector("#lastName-contact").value.trim();
   const email = document.querySelector("#email-contact").value.trim();
   const pet_name = document.querySelector("#petName-contact").value.trim();
-  const type = document.querySelector("#petType-contact").value.trim();
+  const petType = document.querySelector("#petType-contact").value.trim();
   const breed = document.querySelector("#breed-contact").value.trim();
   const birthday = document.querySelector("#birthday-contact").value.trim();
   const sex = document.querySelector("#sex-contact").value.trim();
@@ -161,40 +136,50 @@ const guestFormHandler = async (event) => {
     lastName &&
     email &&
     pet_name &&
-    type &&
+    petType &&
     sex &&
     isNeuteredOrSpayed &&
     vaccinated &&
     concern
   ) {
-    sendRequest(
-      user_id,
-      firstName,
-      lastName,
-      email,
-      pet_name,
-      type,
-      breed,
-      allergies,
-      vaccinated,
-      birthday,
-      isNeuteredOrSpayed,
-      sex,
-      concern
-    );
+    const isUser = false;
+
+    const response = await fetch("/api/appointment/", {
+      method: "POST",
+      body: JSON.stringify({
+        isUser,
+        firstName,
+        lastName,
+        email,
+        pet_name,
+        petType,
+        breed,
+        allergies,
+        vaccinated,
+        birthday,
+        isNeuteredOrSpayed,
+        sex,
+        concern,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.getElementById("inner-content").innerHTML =
+        "Your appointment request has been received! Keep an eye on your email for updates.";
+    } else {
+      alert("Failed to submit appointment");
+    }
   } else {
-    document.getElementById("warning").innerHTML =
-      "You are missing one or more required fields";
+    alert("You are missing one or more required fields");
   }
 };
 
-var guestForm = document.querySelector(".guest-form");
-var clientForm = document.querySelector(".client-form");
+document
+  .querySelector(".guest-form")
+  .addEventListener("submit", guestFormHandler);
 
-if (guestForm) {
-  guestForm.addEventListener("submit", guestFormHandler);
-}
-if (clientForm) {
-  getPets();
-  clientForm.addEventListener("submit", clientFormHandler);
-}
+document
+  .querySelector(".client-form")
+  .addEventListener("submit", clientFormHandler)
+  .addEventListener("DOMContentLoaded", getPets);
